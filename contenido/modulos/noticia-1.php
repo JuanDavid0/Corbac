b205142ea3e847d3076963b0f143332da94a47a0
JuanDavid0 <juanochoa20029128@gmail.com>
2024-08-06 21:08:07 -0500

    implementacion de modulo de noticias y correccion de css para convenios. propuesta de fondo

--- contenido/modulos/noticia-1.php
+++ contenido/modulos/noticia-1.php
@@ -9,33 +9,29 @@ while ($rescontenido = mysqli_fetch_array($consulta_historia_modulo)) {
 }
 ?>
 <div id="noticia-1">
-    <h3 id="noticia-1-titulo"><a href="noticias"><?php echo $varcontenido[0]; ?></a></h3>
-    <div id="noticia-1-contenido">
+    <h3 id="noticia-1-titulo"><?php echo $varcontenido[0]; ?></h3>
+    <div class="noticia-1-contenido">
         <?php
         $consultanoticialim = consultaNoticiasLim($varcontenido[1], $idioma);
         while ($resnoticialim = mysqli_fetch_array($consultanoticialim)) {
+            $fecha = $resnoticialim['fecha'];
+            $timestamp = strtotime($fecha);
+            $dia = date('d', $timestamp);
+            $mes = date('M', $timestamp);
             ?>
-            <div class="noticia-1-esp">
-                <div class="noticia-1-esp-img"
-                    style="background-image: url('<?php echo $rutaFinal; ?>contenido/assets/<?php echo $resnoticialim['imagen_p']; ?>.jpg');">
-                    <div class="noticia-1-esp-capa">
-                        <a href="<?php echo $ruta; ?>blogs/<?php echo $resnoticialim['url_amigable']; ?>">Leer Más...</a>
-                    </div>
-                </div>
-                <div class="noticia-1-esp-contenedor">
-                    <div class="noticia-1-esp-titulo">
-
-                        <a
-                            href="<?php echo $ruta; ?>blogs/<?php echo $resnoticialim['url_amigable']; ?>"><?php echo $resnoticialim['nombre']; ?></a>
-                    </div>
-                    <div class="noticia-1-esp-contenido">
-                        <p><?php echo $resnoticialim['presentacion']; ?></p>
-
-                        <!-- agragado borrar -->
-                        <a href="<?php echo $ruta; ?>blogs/<?php echo $resnoticialim['url_amigable']; ?>">Leer Más</a>
-                    </div>
-                </div>
-            </div>
+            <figure class="noticias">
+                <img src="<?php echo $rutaFinal; ?>contenido/assets/<?php echo $resnoticialim['imagen_p']; ?>"
+                    alt="pr-sample13" />
+                <div class="date"><span class="day"><?php echo $dia; ?></span><span
+                        class="month"><?php echo $mes; ?></span></div>
+                <figcaption>
+                    <h3><?php echo $resnoticialim['nombre']; ?></h3>
+                    <p><?php echo $resnoticialim['presentacion']; ?></p>
+                </figcaption>
+                <div class="hover"><i class="ion-android-open"></i></div>
+                <a href="<?php echo $ruta; ?>blogs/<?php echo $resnoticialim['url_amigable']; ?>"></a>
+            </figure>
         <?php } ?>
     </div>
+
 </div>
