5b943cbdf508ed0866d0abf8aadf0c91c50b41ab
Esteban-Coronado <esteban.coronado0613@gmail.com>
2024-10-02 09:41:20 -0500

    Feat : Primera version de Login y panel administrativo

---
+++ admin-corbac/contenido/clases/ruta.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Description of ruta
+ *
+ * @author Anbu Business Group
+ * Andres Nicolas Lopez Robles
+ */
+class Ruta {
+    public static function retornaRutaAdmin() {
+        //return 'https://megaproyectos.com.co/an-admin/';
+        return 'http://corbac.com./an-admin/';
+    }
+}
---
+++ admin-corbac/contenido/modulos/logAdmin.php
@@ -0,0 +1,26 @@
+<div id="cont-general">
+    <div id="cont-login">
+        <div id="login" style="background-image: url(<?php echo $rutaFinal; ?>contenido/assets/background.jpg)">
+            <div id="login-capa"></div>
+        </div>
+        <div id="contenedor-login">
+            <img src="<?php echo $rutaFinal; ?>contenido/assets/logo.webp" alt="Logo">
+            <h2>Iniciar Sesión</h2>
+            <form class="formulario" method="post" action="" onsubmit="logUsuario(); return false;">                
+                <label class="usuario" for="inputUsuario">Usuario</label>
+                <input id="inputUsuario" class="inputForm" type="text" name="inputUsuario" required>
+                <label class="passw" for="inputContrasena">Contraseña</label>
+                <input id="inputContrasena" class="inputForm" type="password" required>
+                <button class="botonForm" type="submit" value="Ingresar.">Ingresar</button>
+                <div class="alerta">
+                    <div class="alerta-titulo"></div>
+                    <div class="alerta-icono"></div>
+                    <p id="respuestaLogin" class="alerta-texto"></p>
+                    <div class="alerta-boton" onclick="ocultarAlerta()">
+                        <p>Cerrar</p>
+                    </div>	
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
