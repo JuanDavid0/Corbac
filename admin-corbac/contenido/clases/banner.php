7a0524cc2e7783975419a8e16efe1693f483702e
JuanDavid0 <juanochoa20029128@gmail.com>
2024-10-12 00:01:54 -0500

    Se añadieron las opciones de orden y disposicion en el form de editar banner

--- admin-corbac/contenido/clases/banner.php
+++ admin-corbac/contenido/clases/banner.php
@@ -23,7 +23,7 @@ class Banner
     {
         $conn = new Conexion();
         $conexion = $conn->connectDB();
-        $sql = "SELECT * FROM $tabla WHERE identificador_pagina = 'inicio'";
+        $sql = "SELECT * FROM $tabla WHERE identificador_pagina = 'inicio' order by orden";
         $consulta = $conexion->prepare($sql);
         $consulta->execute();
         return $consulta->fetchAll();
@@ -54,36 +54,38 @@ class Banner
         }
     }
     static function crearBanner($tabla, $banner)
-{
-    $conn = new Conexion();
-    $stmt = $conn->connectDB();
-    $consulta = $stmt->prepare("INSERT INTO $tabla (identificador_pagina, 
+    {
+        $conn = new Conexion();
+        $stmt = $conn->connectDB();
+        $consulta = $stmt->prepare("INSERT INTO $tabla (identificador_pagina, 
         imagen, alt, titulo, texto, 
         texto_boton, url, fecha_inicio, fecha_final, orden,
         disposicion, estado, idioma) 
         VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);");
-    if ($consulta->execute([
-        $banner->identificador_pagina,
-        $banner->imagen,
-        $banner->alt,           
-        $banner->titulo,
-        $banner->texto,
-        $banner->texto_boton,
-        $banner->url,
-        $banner->fecha_inicio,
-        $banner->fecha_final,
-        $banner->orden,         
-        $banner->disposicion,
-        $banner->estado,        
-        $banner->idioma
-    ])) {
-        unset($stmt);
-        return 1; 
-    } else {
-        unset($stmt);
-        return 0;
+        if (
+            $consulta->execute([
+                $banner->identificador_pagina,
+                $banner->imagen,
+                $banner->alt,
+                $banner->titulo,
+                $banner->texto,
+                $banner->texto_boton,
+                $banner->url,
+                $banner->fecha_inicio,
+                $banner->fecha_final,
+                $banner->orden,
+                $banner->disposicion,
+                $banner->estado,
+                $banner->idioma
+            ])
+        ) {
+            unset($stmt);
+            return 1;
+        } else {
+            unset($stmt);
+            return 0;
+        }
     }
-}
 
     static function editarBanner($tabla, $banner)
     {
@@ -98,6 +100,8 @@ class Banner
             url = '$banner->url',
             fecha_inicio = '$banner->fecha_inicio',
             fecha_final = '$banner->fecha_final',
+            disposicion = '$banner->disposicion',
+            orden = '$banner->orden',
             estado = '$banner->estado',
             idioma = '$banner->idioma'
             WHERE identificador = '$banner->identificador';");
--- admin-corbac/contenido/modulos/editarbanneradmin.php
+++ admin-corbac/contenido/modulos/editarbanneradmin.php
@@ -9,82 +9,103 @@ $bannerF = Banner::buscarBanner('banner',$testimonio); // se trae toda la infoma
 <div id="contenedor-AreaTrabjo-Admin">
     <div class="contenedor-Agregar-minas"></div>
     <div id="AreaTrabjo-Admin-cont">
-    <h2>EDITAR BANNER</h2>           
-        <form id="contenedor-form-Admin" method="POST" action="<?php echo $rutaFinal ?>contenido/ajax/ajaxBanner.php" enctype="multipart/form-data">                  
+        <h2>EDITAR BANNER</h2>
+        <form id="contenedor-form-Admin" method="POST" action="<?php echo $rutaFinal ?>contenido/ajax/ajaxBanner.php"
+            enctype="multipart/form-data">
             <label class="label-form-act-admin">Imagen de fondo:</label>
             <label class="label-form-act-admin">Tamaño recomendado: 1600px X 600px</label>
-            <input id="inputBannerImagen" name="imagen" class="input-form-act-admin" type="file" accept=".jpg, .webp"/>
+            <input id="inputBannerImagen" name="imagen" class="input-form-act-admin" type="file" accept=".jpg, .webp" />
 
-            <img id="previsua" src="<?php echo $rutaFinalAssets.'contenido/assets/'.$bannerF['imagen']; ?>" style="display: block; width: 100%; height: 200px; background-position: center; background-size:contain; background-repeat:no-repeat; margin:5px auto;"/>
-            
+            <img id="previsua" src="<?php echo $rutaFinalAssets.'contenido/assets/'.$bannerF['imagen']; ?>"
+                style="display: block; width: 100%; height: 200px; background-position: center; background-size:contain; background-repeat:no-repeat; margin:5px auto;" />
+
             <label class="label-form-act-admin">Título:</label>
-            <input name="titulo" class="input-form-act-admin" type="text" required value="<?php echo $bannerF['titulo']; ?>"> 
+            <input name="titulo" class="input-form-act-admin" type="text" required
+                value="<?php echo $bannerF['titulo']; ?>">
 
             <label class="label-form-act-admin">Texto:</label>
-            <input name="texto" class="input-form-act-admin" type="text" required value="<?php echo $bannerF['texto']; ?>">
+            <input name="texto" class="input-form-act-admin" type="text" required
+                value="<?php echo $bannerF['texto']; ?>">
 
             <label class="label-form-act-admin">Texto Botón:</label>
-            <input name="texto_boton" class="input-form-act-admin" type="text" value="<?php echo $bannerF['texto_boton']; ?>">
+            <input name="texto_boton" class="input-form-act-admin" type="text"
+                value="<?php echo $bannerF['texto_boton']; ?>">
 
             <label class="label-form-act-admin">URL del botón:</label>
             <input name="url" class="input-form-act-admin" type="text" value="<?php echo $bannerF['url']; ?>">
 
+            <label class="label-form-act-admin">Ubicación Texto</label>
+            <select name="disposicion" class="input-form-act-admin" required>
+                <option value="0">Izquierda</option>
+                <option value="1" selected="">Centro</option>
+                <option value="2">Derecha</option>
+            </select>
+
+            <label class="label-form-act-admin">Orden:</label>
+            <input name="orden" class="input-form-act-admin" type="number" value="<?php echo $bannerF['orden']; ?>">
+
             <label class="label-form-act-admin">Fecha inicio:</label>
-            <input name="fecha_inicio" class="input-form-act-admin" type="date" value="<?php echo $bannerF['fecha_inicio']; ?>"> 
+            <input name="fecha_inicio" class="input-form-act-admin" type="date"
+                value="<?php echo $bannerF['fecha_inicio']; ?>">
             <label class="label-form-act-admin">Fecha final:</label>
-            <input name="fecha_final" class="input-form-act-admin" type="date" value="<?php echo $bannerF['fecha_final']; ?>">
+            <input name="fecha_final" class="input-form-act-admin" type="date"
+                value="<?php echo $bannerF['fecha_final']; ?>">
 
             <label class="label-form-act-admin">Idioma</label>
             <select name="idioma" class="input-form-act-admin">
                 <option value="es" <?php if($bannerF['idioma'] === 'es' ){ echo 'selected=""'; } ?>>Español</option>
-                <option value="en" <?php if($bannerF['idioma'] === 'en' ){ echo 'selected=""'; } ?>>Inglés</option>               
-            </select> 
+                <option value="en" <?php if($bannerF['idioma'] === 'en' ){ echo 'selected=""'; } ?>>Inglés</option>
+            </select>
 
             <label class="label-form-act-admin">Estado</label>
             <select name="estado" class="input-form-act-admin">
-                <option value="activo" <?php if($bannerF['estado'] === 'activo'){ echo 'selected=""'; } ?>>Activo</option>
-                <option value="inactivo" <?php if($bannerF['estado'] === 'inactivo'){ echo 'selected=""'; } ?>>Inactivo</option>               
-            </select>  
+                <option value="activo" <?php if($bannerF['estado'] === 'activo'){ echo 'selected=""'; } ?>>Activo
+                </option>
+                <option value="inactivo" <?php if($bannerF['estado'] === 'inactivo'){ echo 'selected=""'; } ?>>Inactivo
+                </option>
+            </select>
 
             <input name="accion" value="editar" type="hidden" readonly required>
-            <input name="identificador" value="<?php echo $bannerF['identificador']; ?>" type="hidden" readonly required>
-            <button class="inputSubmitForm" type="submit">Editar</button>   
+            <input name="identificador" value="<?php echo $bannerF['identificador']; ?>" type="hidden" readonly
+                required>
+            <button class="inputSubmitForm" type="submit">Editar</button>
         </form>
     </div>
 </div>
 <script>
-    function mostrarImagen(input) {
-        if (input.files && input.files[0]) {
-            var reader = new FileReader();
-            reader.onload = function (e) {
-                var filePreview = document.getElementById('previsua');
-                filePreview.id = 'file-preview';
-                filePreview.src = e.target.result;
-                var previewZone = document.getElementById('file-preview-zone');
-                previewZone.appendChild(filePreview);
-            };
-            reader.readAsDataURL(input.files[0]);
-        }
+function mostrarImagen(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            var filePreview = document.getElementById('previsua');
+            filePreview.id = 'file-preview';
+            filePreview.src = e.target.result;
+            var previewZone = document.getElementById('file-preview-zone');
+            previewZone.appendChild(filePreview);
+        };
+        reader.readAsDataURL(input.files[0]);
     }
-    function mostrarImagenP(input) {
-        if (input.files && input.files[0]) {
-            var reader = new FileReader();
-            reader.onload = function (e) {
-                var filePreview = document.getElementById('previsuaP');
-                filePreview.id = 'file-preview';
-                filePreview.src = e.target.result;
-                var previewZone = document.getElementById('file-preview-zone');
-                previewZone.appendChild(filePreview);
-            };
-            reader.readAsDataURL(input.files[0]);
-        }
+}
+
+function mostrarImagenP(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            var filePreview = document.getElementById('previsuaP');
+            filePreview.id = 'file-preview';
+            filePreview.src = e.target.result;
+            var previewZone = document.getElementById('file-preview-zone');
+            previewZone.appendChild(filePreview);
+        };
+        reader.readAsDataURL(input.files[0]);
     }
-    var fileUpload = document.getElementById('inputBannerImagen');
-    fileUpload.onchange = function (e) {
-        mostrarImagen(e.srcElement);
-    };
-    var fileUpload = document.getElementById('inputImagenP');
-    fileUpload.onchange = function (e) {
-        mostrarImagenP(e.srcElement);
-    };
+}
+var fileUpload = document.getElementById('inputBannerImagen');
+fileUpload.onchange = function(e) {
+    mostrarImagen(e.srcElement);
+};
+var fileUpload = document.getElementById('inputImagenP');
+fileUpload.onchange = function(e) {
+    mostrarImagenP(e.srcElement);
+};
 </script>
